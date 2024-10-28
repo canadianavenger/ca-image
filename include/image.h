4984a0fcdcad6c2bc067f8fdff785ee37706e7e2
Canadian Avenger <174906062+canadianavenger@users.noreply.github.com>
2024-10-27 20:44:22 -0400

    added core image type files

---
+++ include/image.h
@@ -0,0 +1,43 @@
+/*
+ * bmp.h 
+ * interface definitions for a writing an indexed 256 colour Windows BMP file
+ * 
+ * This code is offered without warranty under the MIT License. Use it as you will 
+ * personally or commercially, just give credit if you do.
+ */
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifndef CA_IMAGE
+#define CA_IMAGE
+
+/// @brief generic palette structure
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} img_pal_entry_t;
+
+/// @brief neutral container for storing paletted images
+typedef struct {
+    unsigned width;      // width of image in pixels
+    unsigned height;     // height of image in pixels
+    unsigned colours;    // number of palette entries
+    size_t   image_size; // size of image in bytes. (also defines the offset to the start of the palette data)
+    img_pal_entry_t  *pal;
+    uint8_t  data[];    // pixel data stored as one byte per pixel
+                        // palette RGB data is appended after image data
+} quick_image_t;
+
+/// @brief allocates and initializes space for an image of the defined size
+/// @param width   width in pixels of the image
+/// @param height  height in pixels of the image
+/// @param colours maximum number of colours the palette will contain
+/// @return returns a pointer to the allocated memory, or NULL
+quick_image_t *image_alloc(unsigned width, unsigned height, unsigned colours);
+
+/// @brief frees the memory allocated using image_alloc()
+/// @param img // pointer to the previously allocated memory
+void image_free(quick_image_t *img);
+
+#endif
---
+++ src/image.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <image.h>
+
+quick_image_t *image_alloc(unsigned width, unsigned height, unsigned colours) {
+    quick_image_t *img = NULL;
+
+    if(NULL == (img = calloc(1, sizeof(quick_image_t) + (width * height) + (colours * sizeof(img_pal_entry_t))) )) {
+        return NULL;
+    }
+
+    img->width = width;
+    img->height = height;
+    img->colours = colours;
+    img->image_size = width * height;
+    img->pal = (img_pal_entry_t  *)&img->data[img->image_size];
+    return img;
+}
+
+// currently this function isn't strictly necessary, a simle free will do
+// but if down the road we decide to alter the underlying structure/allocations
+// this function will handle it appropriately
+void image_free(quick_image_t *img) {
+    if(NULL == img) return;
+    free(img);
+}
